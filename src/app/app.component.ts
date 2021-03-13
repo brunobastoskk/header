fcbce4f6b593112c0385971560a630a98d603a62
paulorochakk <47322682+paulorochakk@users.noreply.github.com>
2021-03-13 20:05:39 +0000

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { Inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
   count = 0;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+
   ngOnInit() {
-    document.querySelector('#addToCart').addEventListener('click', () => {
-      this.count++;
-    })
+    if (isPlatformBrowser(this.platformId)) {
+      document.querySelector('#addToCart').addEventListener('click', () => {
+        this.count++;
+      });
+    }
+    if (isPlatformServer(this.platformId)) {
+      console.log('TEST SERVER')
+    }
   }
 }
